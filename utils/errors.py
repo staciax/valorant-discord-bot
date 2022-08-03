e7376b1a457f471faa2ad745d876c4ffa9e51e56
Dee Dev <6505001948@rumail.ru.ac.th>
2022-08-03 13:11:26 +0700

    v4.0.0 ready to coding soon

--- utils/errors.py
+++ utils/errors.py
@@ -1,45 +1,45 @@
-from discord.app_commands import AppCommandError
+from discord import app_commands
 
 
-class NotOwner(AppCommandError):
+class NotOwner(app_commands.AppCommandError):
     """Raised when a command is used by a user who is not the owner of the bot."""
     pass
 
 
-class BadArgument(AppCommandError):
+class BadArgument(app_commands.AppCommandError):
     """Raised when a command's argument could not be found."""
     pass
 
 
-class ValorantBotError(AppCommandError):
+class ValorantBotError(app_commands.AppCommandError):
     """base class for all errors raised by the bot"""
     pass
 
 
 # https://github.com/colinhartigan/valclient.py/blob/0dcff9e384943a2889e6b3f8e71781c9fc950bce/src/valclient/exceptions.py#L1
 
-class ResponseError(AppCommandError):
+class ResponseError(app_commands.AppCommandError):
     """
     Raised whenever an empty response is given by the Riot server.
     """
     pass
 
 
-class HandshakeError(AppCommandError):
+class HandshakeError(app_commands.AppCommandError):
     """
     Raised whenever there's a problem while attempting to communicate with the local Riot server.
     """
     pass
 
 
-class AuthenticationError(AppCommandError):
+class AuthenticationError(app_commands.AppCommandError):
     """
     Raised whenever there's a problem while attempting to authenticate with the Riot server.
     """
     pass
 
 
-class DatabaseError(AppCommandError):
+class DatabaseError(app_commands.AppCommandError):
     """
     Raised whenever there's a problem while attempting to access the database.
     """
